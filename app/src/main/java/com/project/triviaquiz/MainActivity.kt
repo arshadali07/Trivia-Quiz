55cf6abbf3b66b61ef2871e06b55893acffee4db
arshadali07 <arshadaliabc07@gmail.com>
2026-01-20 22:04:12 +0530

    refactor: Added lifecycle viewmodel dependency internet permission and setup starting screen

--- app/src/main/java/com/project/triviaquiz/MainActivity.kt
+++ app/src/main/java/com/project/triviaquiz/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.project.triviaquiz.presentation.start_quiz.StartQuizRoot
+import com.project.triviaquiz.presentation.start_quiz.StartQuizViewModel
 import com.project.triviaquiz.ui.theme.TriviaQuizTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +29,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun App() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        contentWindowInsets = WindowInsets()
+    ) { innerPadding ->
+        val viewModel = viewModel { StartQuizViewModel() }
+        StartQuizRoot(
+            modifier = Modifier.padding(paddingValues = innerPadding),
+            viewModel = viewModel
+        )
     }
 }
